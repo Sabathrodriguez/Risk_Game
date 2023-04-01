94e018c6d9eb2957f872b43bea1008c3bc55356c
Sabath Rodriguez <sabathrod@gmail.com>
2023-04-01 14:16:10 -0600

    added some connections between territories

--- View/Game.py
+++ View/Game.py
@@ -21,6 +21,8 @@ class Game:
 
         self.root.geometry('1440x1080')
 
+        self.canvas = Canvas(self.root, width=1440, height=1080)
+
         self.myFont = font.Font(size=8)
 
         self.map = map(self.players)
@@ -140,184 +142,216 @@ class Game:
         for k, v in self.map.territories.items():
             h = 2
             w = 18
+            z = 50
+            t = 20 
+            a = 20
+            b = 20
+            c = 20
+            d = 20
+            e = 20
+            f = 20
             if k == 'ALASKA':
                 self.x = 0
                 self.y = 0
                 w = 6
             elif k == 'NORTH WESTERN TERRITORY':
-                self.x = 80
+                self.x = 80 + f
                 self.y = 0
+                #alaska to north western territory
+                self.canvas.create_line(10, 10, 80 + f, 10, width=2)
             elif k == 'GREENLAND':
-                self.x = 230
+                self.x = 230 + 2*f
                 self.y = 0
                 w = 8
+                #north western tetrritory to greenland
+                self.canvas.create_line(80 + f, 10, 230 + 2*f, 10, width=2)
             elif k == 'ICELAND':
-                self.x = 420
+                self.x = 420 + 3*f
                 self.y = 0
                 w = 6
+                #greenland to iceland
+                self.canvas.create_line(230 + 2*f, 10, 420 + 3*f, 10, width=2)
             elif k == 'SCANDINAVIA':
-                self.x = 500
+                self.x = 500 + 4*f
                 self.y = 0
                 w = 10
             elif k == 'SIBERIA':
-                self.x = 840
+                self.x = 840 + 5*f
                 self.y = 0
                 w = 6
             elif k == 'YAKUTSK':
-                self.x = 930
+                self.x = 930 + 6*f
                 self.y = 0
                 w = 6
             elif k == 'KAMCHATKA':
-                self.x = 1010
+                self.x = 1010 + 7*f
                 self.y = 0
                 w = 8
             #-----------------------------#
             elif k == 'ALBERTA':
                 self.x = 0
-                self.y = 30
+                self.y = 30 + t
                 w = 6
+                #alaska to alberta
+                self.canvas.create_line(10, 10, 10, 30 + t, width=2)
+                #north western territory to alberta
+                self.canvas.create_line(80 + f, 10, 50, 30 + t, width=2)
             elif k == 'ONTARIO':
-                self.x = 80
-                self.y = 30
+                self.x = 80 + f
+                self.y = 30 + t
                 w = 6
+                #north western territory to ontario
+                self.canvas.create_line(100 + f, 10, 100 + f, 30 + t, width=2)
+                #greenland to ontario                
+                self.canvas.create_line(230 + 2*f, 10, 100 + f, 30 + t, width=2, fill='red')
             elif k == 'QUEBEC':
-                self.x = 160
-                self.y = 30
+                self.x = 160 + 2*f
+                self.y = 30 + t
                 w = 6
+                #greenland to quebec
+                self.canvas.create_line(230 + 2*f, 10, 190 + 2*f, 30 + t, width=2)
             elif k == 'GREAT BRITAIN':
-                self.x = 420
-                self.y = 30
+                self.x = 420 + 3*f
+                self.y = 30 + t
                 w = 10
             elif k == 'NORTHERN EUROPE':
-                self.x = 520
-                self.y = 30
+                self.x = 520 + 4*f
+                self.y = 30 + t
                 w = 14
             elif k == 'UKRAINE':
-                self.x = 650
-                self.y = 30
+                self.x = 650 + 5*f
+                self.y = 30 + t
                 w = 6
             elif k == 'URAL':
-                self.x = 840
-                self.y = 30
+                self.x = 840 + 6*f
+                self.y = 30 + t
                 w = 4
             elif k == 'IRKUTSK':
-                self.x = 910
-                self.y = 30
+                self.x = 910 + 7*f
+                self.y = 30 + t
                 w = 8
             elif k == 'MONGOLIA':
-                self.x = 1000
-                self.y = 30
+                self.x = 1000 + 8*f
+                self.y = 30 + t
                 w = 8
             elif k == 'JAPAN':
-                self.x = 1090
-                self.y = 30
+                self.x = 1090 + 9*f
+                self.y = 30 + t
                 w = 6
             #-----------------------------#
             elif k == 'WESTERN UNITED STATES':
                 self.x = 0
-                self.y = 60
+                self.y = 60 + t + a
 
             elif k == 'EASTERN UNITED STATES':
-                self.x = 140
-                self.y = 60
+                self.x = 140 + f
+                self.y = 60 + t + a
+            elif k == 'WESTERN EUROPE':
+                self.x = 420 + 2*f
+                self.y = 60 + t + a
+                w = 12
+            elif k == 'SOUTHERN EUROPE':
+                self.x = 540 + 3*f
+                self.y = 60 + t + a
+                w = 12
             elif k == 'AFGHANISTAN':
-                self.x = 840
-                self.y = 60
+                self.x = 840 + 4*f
+                self.y = 60 + t + a
                 w = 8
             elif k == 'CHINA':
-                self.x = 940
-                self.y = 60
+                self.x = 940 + 5*f
+                self.y = 60 + t + a
                 w = 4
             #-----------------------------#
             elif k == 'CENTRAL AMERICA':
-                self.x = 0
-                self.y = 90
+                self.x = 0 
+                self.y = 90 + t + a + b
                 w = 12
-            elif k == 'WESTERN EUROPE':
-                self.x = 420
-                self.y = 90
-                w = 12
-            elif k == 'SOUTHERN EUROPE':
-                self.x = 540
-                self.y = 90
-                w = 12
+                self.canvas.create_line(10, 90 + t + a + b, 10, 120 + z + t + a + b, width=2)
+                self.canvas.pack()
             elif k == 'MIDDLE EAST':
-                self.x = 840
-                self.y = 90
+                self.x = 840 + f
+                self.y = 90 + t + a + b
                 w = 8
             elif k == 'INDIA':
-                self.x = 930
-                self.y = 90
+                self.x = 930 + 2*f
+                self.y = 90 + t + a + b
                 w = 4
             elif k == 'SLAM':
-                self.x = 1000
-                self.y = 90
-                w = 4
-            #-----------------------------#
+                self.x = 1000 + 3*f
+                self.y = 90 + t + a + b
+                w = 4            
+            #-----------------------------#            
             elif k == 'VENEZUELA':
                 self.x = 0
-                self.y = 120
+                self.y = 120 + z + t + a + b
                 w = 8
             elif k == 'NORTH AFRICA':
-                self.x = 540
-                self.y = 120
+                self.x = 540 + f
+                self.y = 120 + z + t + a + b
+                #brazil to north africa
+                self.canvas.create_line(150, 150 + z + t + a + b + c, 540 + f, 130 + z + t + a + b, width=2)
                 w = 10
             elif k == 'EGYPT':
-                self.x = 640
-                self.y = 120
+                self.x = 640 + 2*f
+                self.y = 120 + z + t + a + b
                 w = 6
             elif k == 'INDONESIA':
-                self.x = 930
-                self.y = 120
+                self.x = 930 + 3*f
+                self.y = 120 + z + t + a + b
                 w = 10
             elif k == 'NEW GUINEA':
-                self.x = 1200
-                self.y = 120
+                self.x = 1100 + 4*f
+                self.y = 120 + z + t + a + b
                 w = 10
             #-----------------------------#
             elif k == 'BRAZIL':
-                self.x = 200
-                self.y = 150
+                self.x = 100
+                self.y = 150 + z + t + a + b + c
                 w = 6
+                self.canvas.create_line(0, 120 + z + t + a + b, 100, 150 + z + t + a + b + c, width=2)
             elif k == 'CONGO':
-                self.x = 540
-                self.y = 150
+                self.x = 540 + f
+                self.y = 150 + z + t + a + b + c
                 w = 4
             elif k == 'EAST AFRICA':
-                self.x = 640
-                self.y = 150
+                self.x = 640 + 2*f
+                self.y = 150 + z + t + a + b + c
                 w = 8
             elif k == 'WESTERN AUSTRALIA':
-                self.x = 930
-                self.y = 150
+                self.x = 930 + 3*f
+                self.y = 150 + z + t + a + b + c
                 w = 16
             elif k == 'EASTERN AUSTRALIA':
-                self.x = 1200
-                self.y = 150
+                self.x = 1100 + 4*f
+                self.y = 150 + z + t + a + b + c
                 w = 16
             #-----------------------------#
             elif k == 'PERU':
                 self.x = 0
-                self.y = 180
+                self.y = 180 + z + t + a + b + c + d
                 w = 4
+                #venezuela to peru
+                self.canvas.create_line(10, 120 + z + t + a + b, 10, 180 + z + t + a + b + c + d, width=2)
+                #brazil to peru
+                self.canvas.create_line(100, 150 + z + t + a + b + c, 10, 180 + z + t + a + b + c + d, width=2)
             elif k == 'SOUTH AFRICA':
-                self.x = 540
-                self.y = 180
+                self.x = 540 + f
+                self.y = 180 + z + t + a + b + c + d
                 w = 10
             elif k == 'MADAGASCAR':
-                self.x = 640
-                self.y = 180
+                self.x = 640 + 2*f
+                self.y = 180 + z + t + a + b + c + d
                 w = 8
             #-----------------------------#
             elif k == 'ARGENTINA':
                 self.x = 0
-                self.y = 210
-                w = 8
-            # if (self.i % 10) == 0:
-            #     self.x += 200
-            #     self.y = 0
-            # else:
-            #     self.y += 35            
+                self.y = 210 + z + t + a + b + c + d + e
+                w = 8    
+                #peru to argentina
+                self.canvas.create_line(10, 180 + z + t + a + b + c + d, 10, 210 + z + t + a + b + c + d + e, width=2)
+                #brazil to argentina
+                self.canvas.create_line(100, 150 + z + t + a + b + c, 60, 210 + z + t + a + b + c + d + e, width=2)
 
             self.i += 1
             t = Button(self.root, text = str(k) + " " + str(self.map.territories[k].numberOfTroops) + '\n' + (self.map.territories[k].player if self.map.territories[k].player is not None else ''), height=h, width=w, bd = '2', command = partial(self.selectTerritory, k))
